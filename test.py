812dfeb66f6cfbd110d179dd511094db11aa8a3b
Wannaphong <wannaphong@kkumail.com>
2019-05-08 19:06:21 +0700

    show dep

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
 # coding: utf8
 import spacy
+from pathlib import Path
+from spacy import displacy
 output_dir="./data2"
 nlp = spacy.load(output_dir)
 while True:
  text=input("text : ")
  doc = nlp(text)
  print("Dependencies", [(t.text, t.dep_, t.head.text) for t in doc])
+ svg = displacy.render(doc, style="dep")
+ output_path = Path("./sentence.svg")
+ output_path.open("w", encoding="utf-8").write(svg)
